3fb7a1bcf3f5f0de6fd0ffd00cf1ddcf4d1a43dc
AdrianRocha <130408696+AdrianRochaC@users.noreply.github.com>
2025-05-30 00:47:19 +0000

    hasta el app falte de registro en adelante

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// firebase/firebaseConfig.js
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVEPL_X1Nc6_tHD1nueBfXfWkj_x5vghE",
+  authDomain: "marvelapi-af75c.firebaseapp.com",
+  projectId: "marvelapi-af75c",
+  storageBucket: "marvelapi-af75c.firebasestorage.app",
+  messagingSenderId: "445655726063",
+  appId: "1:445655726063:web:d408ddb0aada8fe899a81a"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
